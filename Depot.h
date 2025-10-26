b79db72c807fc82b180b8d714012b843922a9eaf
shanecsbu <shane.callaghan@stonybrook.edu>
2025-10-26 19:44:37 -0400

    Created Depot.h for Options 12-15

---
+++ Depot.h
@@ -0,0 +1,5 @@
+// Necessary for Multiple Drones
+void copyDrone(int indexFrom, int indexTo);      // (option 12)
+void printAllNames() const;                      // (option 13)
+void sortDroneDataAscending(int droneIdx);       // (option 14)
+void sortDroneDataDescending(int droneIdx);      // (option 15)
