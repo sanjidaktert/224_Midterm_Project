2d5d056cc0ed5207b16dc1830d7a5f4bf554d11f
shanecsbu <shane.callaghan@stonybrook.edu>
2025-10-26 17:48:08 -0400

    Added the void functions to drone.h

--- drone.h
+++ drone.h
@@ -6,53 +6,58 @@ using namespace std;
 
 class drone {
 private:
-	string name; //name of the drone
-	int id; //id of the drone
-	string tasks[5]; //last 5 delivery task names
-	int init_position[2]; //(x,y) location
-	int task_positions[5][2]; //delivery position for each delivery task
+string name; //name of the drone
+int id; //id of the drone
+string tasks[5]; //last 5 delivery task names
+int init_position[2]; //(x,y) location
+int task_positions[5][2]; //delivery position for each delivery task
 
 public:
-	//Constructors
-	drone();
-	drone(const string& name, int id, const string& tasks, int init_position[2], int task_positions[5][2]);
+//Constructors
+drone();
+drone(const string& name, int id, const string& tasks, int init_position[2], int task_positions[5][2]);
 
-	//Getters
-	string getName();
-	int getId();
-	string getTasks();
-	int getInit();
-	int taskPos();
+//Getters
+string getName();
+int getId();
+string getTasks();
+int getInit();
+int taskPos();
 
-	//Setters
-	void setName(const string& n);
-	void setId(int id);
-	void setTasks(const string t[5]);
-	void setInit(const int pos[2]);
-	void setTaskPos(const int pos[5][2]);
+//Setters
+void setName(const string& n);
+void setId(int id);
+void setTasks(const string t[5]);
+void setInit(const int pos[2]);
+void setTaskPos(const int pos[5][2]);
 
-	//Index setters
-	//maps a task name to tasks[]
-	void setTasks(int index, const string& task); 
-	//sets the initial (x,y) position
-	void setInit(int x, int y);
-	//sets the drone's position for a specific task
-	void setTaskPos(int index, int x, int y);
+//Index setters
+//maps a task name to tasks[]
+void setTasks(int index, const string& task); 
+//sets the initial (x,y) position
+void setInit(int x, int y);
+//sets the drone's position for a specific task
+void setTaskPos(int index, int x, int y);
 
-	//Sorting 
-	//void sortByAscending();
-	//void sortByDescending();
-	void sortByName();
-	void sortById();
-	void sortByTasks();
-	void sortByInit();
-	void sortByTaskPos();
+//Sorting 
+//void sortByAscending();
+//void sortByDescending();
+void sortByName();
+void sortById();
+void sortByTasks();
+void sortByInit();
+void sortByTaskPos();
 
-	//Operator overloads
-	void operator<<(drone& d2); //copies all attributes
-	void operator-(drone& d2); //swaps the tasks and their positions
+//Operator overloads
+void operator<<(drone& d2); //copies all attributes
+void operator-(drone& d2); //swaps the tasks and their positions
 
-	//Display
-	void displayDrone(); //displays all the attributes 
+//Display
+// Add these to your public section if they aren't already there:
+void sortByAscending();    // alphabetical A->Z on tasks[], keep positions paired
+void sortByDescending();   // Z->A on tasks[], keep positions paired
+void operator<<(const drone& d2);  // copy ALL attributes from d2 into *this
+void displayDrone() const;         // pretty print (used when retrieving or debugging)
+
 };
 #endif 
